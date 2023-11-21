133debb0aa9da6ac0305b5e8987a3f3f01d27b3d
Lionel Verano <lionel.verano.iv@berkeley.edu>
2023-11-21 15:34:58 -0800

    updated quiz

--- script.js
+++ script.js
@@ -1,63 +1,123 @@
 const questions = [
     {
-        question: "Which of these is the state flower of California?",
-        options: ["Rose", "Tulip", "Golden Poppy", "Sunflower"],
-        answer: "Golden Poppy"
+        question: "What habitat does the California Red-legged Frog primarily occupy?",
+        options: ["Deserts", "High mountains", "Wetlands and streams", "Urban areas"],
+        answer: "Wetlands and streams",
+        source: "California Herps, californiaherps.com/identification/frogsid/frogs.id.html"
     },
     {
-        question: "Which mammal is known as the state marine mammal of California?",
-        options: ["California Sea Lion", "Blue Whale", "Dolphin", "Orca"],
-        answer: "California Sea Lion"
+        question: "Which frog species is sensitive to altered water courses?",
+        options: ["Pacific Tree Frog", "California Treefrog", "Foothill Yellow-legged Frog", "Sierra Nevada Yellow-legged Frog"],
+        answer: "Foothill Yellow-legged Frog",
+        source: "California Herps, californiaherps.com/identification/frogsid/frogs.id3.html"
     },
     {
-        question: "Which is the largest tree by volume in California?",
-        options: ["Douglas Fir", "California Redwood", "Giant Sequoia", "Oak"],
-        answer: "Giant Sequoia"
+        question: "Where can you primarily find the Sierra Nevada Yellow-legged Frog?",
+        options: ["Urban parks", "Mountain lakes", "Coastal areas", "Deserts"],
+        answer: "Mountain lakes",
+        source: "Gone Froggin, gonefroggin.com/2016/09/26/frogs-toads-california"
     },
     {
-        question: "Which large marine mammal is known to migrate along the California coast?",
-        options: ["Orca", "Blue Whale", "Manatee", "Dolphin"],
-        answer: "Blue Whale"
+        question: "What is a distinctive behavior of the Arroyo Toad?",
+        options: ["Changes color frequently", "Lives in trees", "Burrows into the sand", "Migrates long distances"],
+        answer: "Burrows into the sand",
+        source: "SnakeTracks.com, snaketracks.com/frogs-in-california"
     },
     {
-        question: "The California condor is known as one of the world's...",
-        options: ["Smallest birds", "Most colorful birds", "Rarest birds", "Loudest birds"],
-        answer: "Rarest birds"
+        question: "What unique feature is the Pacific Tree Frog known for?",
+        options: ["Loud chorus calls", "Venomous skin", "Extremely long legs", "Ability to fly short distances"],
+        answer: "Loud chorus calls",
+        source: "National Park Service, nps.gov/samo/learn/nature/californiatreefrog.htm"
     },
     {
-        question: "The California Dogface Butterfly is named for...",
-        options: ["Its bark", "The pattern on its wings", "Its diet", "Its size"],
-        answer: "The pattern on its wings"
+        question: "Which California frog is known for its impressive leaping abilities?",
+        options: ["Sierra Nevada Yellow-legged Frog", "Arroyo Toad", "California Red-legged Frog", "Pacific Tree Frog"],
+        answer: "California Red-legged Frog",
+        source: "California Herps, californiaherps.com/identification/frogsid/frogs.id.html"
     },
     {
-        question: "What is the state freshwater fish of California?",
-        options: ["Golden Trout", "Salmon", "Bass", "Bluegill"],
-        answer: "Golden Trout"
+        question: "What is the primary diet of the Foothill Yellow-legged Frog?",
+        options: ["Small fish", "Insects", "Algae", "Berries"],
+        answer: "Insects",
+        source: "California Herps, californiaherps.com/identification/frogsid/frogs.id3.html"
     },
     {
-        question: "Which desert covers a significant portion of southeastern California?",
-        options: ["Mojave Desert", "Sonoran Desert", "Atacama Desert", "Kalahari Desert"],
-        answer: "Mojave Desert"
+        question: "What major threat has contributed to the endangerment of the Sierra Nevada Yellow-legged Frog?",
+        options: ["Habitat loss and disease", "Over-predation", "Climate change", "Pollution"],
+        answer: "Habitat loss and disease",
+        source: "Gone Froggin, gonefroggin.com/2016/09/26/frogs-toads-california"
     },
     {
-        question: "Which is the tallest and fastest bird of prey found in California?",
-        options: ["Golden Eagle", "Peregrine Falcon", "Bald Eagle", "Red-tailed Hawk"],
-        answer: "Peregrine Falcon"
+        question: "Which feature is distinctive of the Arroyo Toad's appearance?",
+        options: ["Bright red eyes", "Spotted skin", "Nocturnal burrowing", "Long, striped legs"],
+        answer: "Nocturnal burrowing",
+        source: "SnakeTracks.com, snaketracks.com/frogs-in-california"
     },
     {
-        question: "Which reptile is known as the state reptile of California?",
-        options: ["Desert Tortoise", "Green Anole", "Gila Monster", "California King Snake"],
-        answer: "Desert Tortoise"
+        question: "How does the Pacific Tree Frog adapt to different environments?",
+        options: ["By hibernating", "By changing its skin color", "By migrating", "By altering its diet"],
+        answer: "By changing its skin color",
+        source: "National Park Service, nps.gov/samo/learn/nature/californiatreefrog.htm"
     },
     {
-        question: "Which is the state insect of California?",
-        options: ["Honeybee", "Ladybug", "Monarch Butterfly", "Praying Mantis"],
-        answer: "California Dogface Butterfly"
+        question: "What unique feature distinguishes the California Treefrog?",
+        options: ["Its loud vocalizations", "Its ability to change color", "Its exceptionally long legs", "Its nocturnal habits"],
+        answer: "Its ability to change color",
+        source: "California Herps, californiaherps.com/identification/frogsid/frogs.id.html"
     },
     {
-        question: "Which tree is known to live for thousands of years in California?",
-        options: ["Palm", "Bristlecone Pine", "Eucalyptus", "Cypress"],
-        answer: "Bristlecone Pine"
+        question: "Which habitat is primarily occupied by the Foothill Yellow-legged Frog?",
+        options: ["Desert areas", "High mountain lakes", "Streams and rivers", "Urban parks"],
+        answer: "Streams and rivers",
+        source: "California Herps, californiaherps.com/identification/frogsid/frogs.id3.html"
+    },
+    {
+        question: "What is the conservation status of the Sierra Nevada Yellow-legged Frog?",
+        options: ["Least Concern", "Endangered", "Vulnerable", "Near Threatened"],
+        answer: "Endangered",
+        source: "Gone Froggin, gonefroggin.com/2016/09/26/frogs-toads-california"
+    },
+    {
+        question: "Which type of area does the Arroyo Toad prefer for its habitat?",
+        options: ["Coastal streams", "Deep forests", "Urban areas", "Mountainous regions"],
+        answer: "Coastal streams",
+        source: "SnakeTracks.com, snaketracks.com/frogs-in-california"
+    },
+    {
+        question: "What behavior is characteristic of the Pacific Tree Frog during mating season?",
+        options: ["Loud chorus calls", "Color changing", "Migration to water bodies", "Digging burrows"],
+        answer: "Loud chorus calls",
+        source: "National Park Service, nps.gov/samo/learn/nature/californiatreefrog.htm"
+    },
+    {
+        question: "Which frog is known for its distinctive red underside and powerful legs?",
+        options: ["Pacific Tree Frog", "California Red-legged Frog", "Foothill Yellow-legged Frog", "Arroyo Toad"],
+        answer: "California Red-legged Frog",
+        source: "California Herps, californiaherps.com/identification/frogsid/frogs.id.html"
+    },
+    {
+        question: "The Foothill Yellow-legged Frog's breeding is closely linked to what natural event?",
+        options: ["Full moon", "Seasonal rains", "Forest fires", "River flooding"],
+        answer: "Seasonal rains",
+        source: "California Herps, californiaherps.com/identification/frogsid/frogs.id3.html"
+    },
+    {
+        question: "What adaptation helps the Sierra Nevada Yellow-legged Frog survive in its high-altitude habitat?",
+        options: ["Webbed feet", "Toxic skin", "Ability to hibernate", "Unique breeding cycle"],
+        answer: "Unique breeding cycle",
+        source: "Gone Froggin, gonefroggin.com/2016/09/26/frogs-toads-california"
+    },
+    {
+        question: "Arroyo Toads are primarily active during which part of the day?",
+        options: ["Morning", "Afternoon", "Evening", "Night"],
+        answer: "Night",
+        source: "SnakeTracks.com, snaketracks.com/frogs-in-california"
+    },
+    {
+        question: "The Pacific Tree Frog can be predominantly found in what type of ecosystem?",
+        options: ["Deserts", "Mountainous regions", "Urban settings", "Wetlands"],
+        answer: "Wetlands",
+        source: "National Park Service, nps.gov/samo/learn/nature/californiatreefrog.htm"
     }
 ];
 
